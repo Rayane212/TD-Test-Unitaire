1ae1d88a6aa1bb6325d10b0f1a68cc96ac0f725f
Rayane212 <rayanehadi41@gmail.com>
2025-04-08 14:37:09 +0200

    Add unit tests for FizzBuzz class

---
+++ FizzBuzz/fizzbuzz_test.py
@@ -0,0 +1,21 @@
+import unittest
+from fizzbuzz import FizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
+    def test_fizzbuzz(self):
+        expected_output = [
+            1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz",
+            11, "Fizz", 13, 14, "FizzBuzz"
+        ]
+        result = list(self.fizzbuzz.fizzbuzz(15))
+        self.assertEqual(result, expected_output)
+    
+    def test_fizzbuzz_last_value(self):
+        result = list(self.fizzbuzz.fizzbuzz(2000000001))
+        self.assertEqual(result[-1], "FizzBuzz")
+   
+    if __name__ == "__main__":
+        unittest.main()
